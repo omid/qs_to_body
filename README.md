3c74b11de17ffee4153213b30274bfb8b416af5c
Omid Rad <omidmr@gmail.com>
2025-11-28 15:59:56 +0100

    Support non-text files & avoid misuse of the service

--- README.md
+++ README.md
@@ -8,10 +8,17 @@ This service basically:
   - `t` → response `Content-Type` (Defaults to `text/html; charset=utf-8`)
   - `b` → response body (Defaults to empty string)
 
+If `Content-Type` is anything other than `text/*`, `b` should be sent as `url_encode(base64_encode(b))`. It means `b` will be url decoded and then base64 decoded.
+
 The service is hosted in [Leapcell](https://leapcell.io/) services for free.
 
-You can find it here: https://qs-to-body.leapcell.app/
+You can find it here: [https://qs-to-body.leapcell.app/](https://qs-to-body.leapcell.app/)
 
 ## Example
 
 [https://qs-to-body.leapcell.app/?b=%3Ch1%3EHello+World!%3C%2Fh1%3E](https://qs-to-body.leapcell.app/?b=%3Ch1%3EHello+World!%3C%2Fh1%3E)
+
+## Limitations
+
+- Unfortunately, to avoid misuse, you are not allowed to have a link to this page in your pages. Either as sourc of a js script or as an image or anything else.
+- There is a limitation for the body, but it depends on the Leapcell server or any other server you run the code on.
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
-use std::str::FromStr;
-
 use axum::{
     Router,
     extract::RawQuery,
-    http::{HeaderName, header},
+    http::{HeaderName, HeaderValue, Method, header},
     response::IntoResponse,
     routing::get,
 };
+use base64::{Engine as _, prelude::BASE64_STANDARD};
+use tower_http::cors::CorsLayer;
 use url::form_urlencoded;
 
 async fn root(RawQuery(raw): RawQuery) -> impl IntoResponse {
     let mut content_type = "text/html; charset=utf-8".to_string();
-    let mut body = String::new();
+    let mut body = Vec::new();
 
     if let Some(q) = raw {
         for (key, value) in form_urlencoded::parse(q.as_bytes()) {
             match &*key {
                 "t" => content_type = value.into_owned(),
-                "b" => body = value.into_owned(),
+                "b" => body = value.into_owned().into_bytes(),
                 _ => {}
             }
         }
+    }
+
+    if !content_type.starts_with("text/")
+        && let Ok(decoded) = BASE64_STANDARD.decode(&body)
+    {
+        body = decoded;
     }
 
     (
         [
             (header::CONTENT_TYPE, content_type),
             (
-                HeaderName::from_str("X-THANKS").unwrap(),
+                HeaderName::from_static("X-THANKS"),
                 "Thanks to Leapcell for hosting.".to_string(),
             ),
         ],
@@ -37,7 +43,16 @@ async fn root(RawQuery(raw): RawQuery) -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let allowed_origins: [HeaderValue; 2] = [
+        "http://127.0.0.1:3000".parse().unwrap(),
+        "https://qs-to-body.leapcell.app/".parse().unwrap(),
+    ];
+
+    let cors = CorsLayer::new()
+        .allow_origin(allowed_origins)
+        .allow_methods([Method::GET, Method::OPTIONS]);
+
+    let app = Router::new().route("/", get(root)).layer(cors);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
